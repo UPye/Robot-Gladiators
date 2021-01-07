6513e44663a3b84a5f83447c5837977331286f1d
UPye <75226236+UPye@users.noreply.github.com>
2021-01-07 16:52:22 -0500

    added the ability to save high score to local storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,7 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+
 // Function to set name
 var getPlayerName = function() {
     var name = "";
@@ -125,27 +126,36 @@ startGame();
 
 // Function to end the entire game
 var endGame = function() {
-    // If player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score");
+    window.alert("The game has now ended. Let's see how you did!");
+  
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+      highScore = 0;
     }
+    // if player have more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+      localStorage.setItem("highscore", playerInfo.money);
+      localStorage.setItem("name", playerInfo.name);
+  
+      alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    } 
     else {
-        window.alert("You've lost your robot in battle.");
+      alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
 
-// Ask player ig they'd like to play again
-var playAgainConfirm = window.confirm("Would you like to play again?");
-
-if (playAgainConfirm) {
-    // Restart the game
-    startGame();
-}
-else {
-    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+  
+    if (playAgainConfirm) {
+      startGame();
+    } 
+    else {
+      window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
-
-};
-    // Shop function
+  };
+  
+// Shop function
     var shop = function() {
         // Ask player what they'd like to do
         var shopOptionPrompt = window.prompt(
